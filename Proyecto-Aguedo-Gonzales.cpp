04733c8efbf00208a354257d5b9e295fae6c94c1
Franz <franz.aguedo.l@uni.pe>
2025-12-04 13:00:40 -0500

    Subido: aplicar stash tras merge

--- Proyecto-Aguedo-Gonzales.cpp
+++ Proyecto-Aguedo-Gonzales.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct EstrucNotas{
+    float* nota;
+    string curso;
+    string estudiante;
+};
+
+void liberarMemoria(EstrucNotas* notas, int cantidadNotas) {
+    for (int i = 0; i < cantidadNotas; ++i) {
+        delete[] notas[i].nota;
+    }
+    delete[] notas; 
+}
+
 
 int main() {
 
